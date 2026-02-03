6ca3e36ab4d11b56014a4ac1816b968a2a9fdf89
glaboy02 <glaboy02@gmail.com>
2026-02-03 11:54:27 -0500

    Update PlayerController.cs

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 5f;
-    private float rotationSpeed = 100f;
+    // private float rotationSpeed = 100f;
     private float horizontalInput;
     private float verticalInput;
+    // private float xRange = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +19,15 @@ public class PlayerController : MonoBehaviour
     {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
+
+        // if (transform.position.x < -xRange)
+        // {
+        //     transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        // }
+        // if (transform.position.x > xRange)
+        // {
+        //     transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        // }
 
         // Vector3 movement = new Vector3(horizontalInput * speed * Time.deltaTime, 0f, 0f);
         // transform.Translate(movement);
