4253da34ca903a73c599074073bb7be6813e89f6
glaboy02 <glaboy02@gmail.com>
2026-02-05 16:45:45 -0500

    Update PlayerController.cs

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -13,6 +13,7 @@ namespace RPG.Character
         private Animator playerAnim;
         private SpriteRenderer playerSprite;
 
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
