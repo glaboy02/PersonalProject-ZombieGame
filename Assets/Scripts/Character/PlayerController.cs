30e8264f26151d985a62e3e304c4ff3e58bad00e
glaboy02 <glaboy02@gmail.com>
2026-02-05 08:09:36 -0500

    Trying to add animations, NOT WORKING

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -7,11 +7,12 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     // private float xRange = 5f;
+    private Animator playerAnim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,10 +32,7 @@ public class PlayerController : MonoBehaviour
 
         // Vector3 movement = new Vector3(horizontalInput * speed * Time.deltaTime, 0f, 0f);
         // transform.Translate(movement);
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-
-
-        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+        
         // transform.Translate(Vector3.up * speed * Time.deltaTime);
 
 
@@ -45,5 +43,17 @@ public class PlayerController : MonoBehaviour
         // );
 
         // transform.Translate(movement * speed * Time.deltaTime);
+
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            playerAnim.SetFloat("Speed", 1f);
+
+            transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+            transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+        }
+        else
+        {
+            playerAnim.SetFloat("Speed", 0f);
+        }
     }
 }
